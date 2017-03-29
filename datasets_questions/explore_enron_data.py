161e28d48d2d8a77158eedb19abd7b4f17c75202
Shawn Q Jackson <gt50@hotmail.com>
2017-03-29 00:14:41 -0700

    finished "explore enron data"

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,24 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print "data points in enron_data: ", len(enron_data)
+#print "features: ", enron_data[0]
+poi = 0
+salaried_employees = 0
+email_addresses = 0
+nan_total_payments = 0
+for person in enron_data:
+    if enron_data[person]["salary"] != "NaN":
+        salaried_employees += 1
+    if enron_data[person]["email_address"] != "NaN":
+        email_addresses += 1
+    if enron_data[person]["total_payments"] == "NaN":# and enron_data[person]["poi"] == True:
+        nan_total_payments += 1
+    if enron_data[person]["poi"] == True:
+        poi += 1
 
+print "salaried employees: ", salaried_employees
+print "email addresses: ", email_addresses
+print "NaN total payments for poi: ", nan_total_payments
+print "poi", poi
+print enron_data["SKILLING JEFFREY K"]
