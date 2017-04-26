2d566c536b0c2753f902be270896f77b64f98380
Shawn Q Jackson <gt50@hotmail.com>
2017-04-26 01:17:33 -0700

    completed enron_outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,12 +9,13 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+print data_dict
+data_dict.pop('TOTAL')
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
-
 
 
 for point in data:
