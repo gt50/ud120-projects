439196c9d51fab703857f6ec944fb5c7ab33714b
Shawn Q Jackson <gt50@hotmail.com>
2017-05-22 22:29:18 -0700

    vectorize_text

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -42,27 +42,32 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
-        if temp_counter < 200:
+        if temp_counter:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            email_text =  parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            names_to_remove = ["sara", "shackleton", "chris", "germani"]
+            for name_to_remove in names_to_remove:
+                email_text.replace(name_to_remove, "")
             ### append the text to word_data
-
+            word_data.append(email_text)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
 print "emails processed"
 from_sara.close()
 from_chris.close()
-
+print word_data[152]
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
